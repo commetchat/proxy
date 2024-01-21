f7a81e67fabe670a129c2320db5ba09a592e7c9a
Airyz <36567925+Airyzz@users.noreply.github.com>
2024-01-22 09:20:23 +1030

    Implement encrypted url preview

--- src/router.ts
+++ src/router.ts
@@ -1,6 +1,9 @@
 import { Router } from 'itty-router';
 import tenor from './proxy_tenor';
 import signal from './proxy_signal';
+import { encryptedPreview } from './encrypted_preview/encrypted_preview';
+import { encryptedPreviewImage } from './encrypted_preview/encrypted_preview_image';
+
 const router = Router();
 
 router.get('/proxy/tenor/api/:path+', ({ params, query }, env: Env) => {
@@ -14,6 +17,15 @@ router.get('/proxy/tenor/media/:path+', ({ params, query }, env: Env) => {
 router.get('/proxy/signal/stickers/:path+', ({ params, query }, env: Env) => {
     return signal.proxyStickers(params, query as { [key: string]: string }, env);
 });
+
+router.get('/url_preview/encrypted/metadata/:userKey/:encryptedUrl', ({ params, query, url }, env: Env) => {
+    return encryptedPreview(params, query as { [key: string]: string }, url, env);
+});
+
+router.get('/url_preview/encrypted/image/:userKey/:encryptedUrl', ({ params, query }, env: Env) => {
+    return encryptedPreviewImage(params, query as { [key: string]: string }, env);
+});
+
 
 router.all('*', () => new Response("Not found", { status: 404 }));
 
