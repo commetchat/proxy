828be5c2e68f6ee9eda308cb713bed46596ac41c
Airyz <36567925+Airyzz@users.noreply.github.com>
2024-04-18 15:12:11 +0930

    Revert "Merge pull request #4 from commetchat/feat/url-preview"
    This reverts commit 7aeb04fc91fa056d97377af5cbf6f4fd2f888b92, reversing
    changes made to 31a116b571747a6a62a168c0ca857f33b5c3621b.

--- src/utils/array.ts
+++
@@ -1,9 +0,0 @@
-export default {
-
-    joinArray(a: Uint8Array, b: Uint8Array): Uint8Array {
-        var mergedArray = new Uint8Array(a.length + b.length);
-        mergedArray.set(a);
-        mergedArray.set(b, a.length);
-        return mergedArray
-    }
-}
